c80e07f7361a8b36eb2f3fba0948cdd2ad93e3d8
Xiong Vang <vang0641@gmail.com>
2017-11-03 15:24:08 -0500

    database setup with org and admin

--- database.sql
+++ database.sql
@@ -1,8 +1,32 @@
--- DB Name: mn-trails-finder
+-- ** Create Database ** --
+-- Make sure database already exists or create it
+-- CREATE DATABASE the_show;
 
+-- ** Drop Foreign Key Contraints
+ALTER TABLE IF EXISTS ad
+    DROP CONSTRAINT IF EXISTS organization_fk;
+ALTER TABLE IF EXISTS ad
+    DROP CONSTRAINT IF EXISTS category_fk;
+ALTER TABLE IF EXISTS media
+    DROP CONSTRAINT IF EXISTS ad_fk;
+ALTER TABLE IF EXISTS credit
+    DROP CONSTRAINT IF EXISTS ad_fk;
+ALTER TABLE IF EXISTS social_media
+    DROP CONSTRAINT IF EXISTS organization_fk;
+ALTER TABLE IF EXISTS social_media
+    DROP CONSTRAINT IF EXISTS social_media_type_fk; 
 
--- Table Creation --
+-- ** Drop Tables** --
+DROP TABLE IF EXISTS organization;
+DROP TABLE IF EXISTS admin;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS media;
+DROP TABLE IF EXISTS ad;
+DROP TABLE IF EXISTS credit;
+DROP TABLE IF EXISTS social_media_type;
+DROP TABLE IF EXISTS social_media;
 
+-- ** Create Tables ** --
 -- houses ad agency information and auth credentials --
 CREATE TABLE organization (
     id SERIAL PRIMARY KEY,
@@ -10,11 +34,21 @@ CREATE TABLE organization (
     description TEXT,
     website VARCHAR(200),
     logo VARCHAR(200),
+    email VARCHAR(200) UNIQUE,
+    password VARCHAR(200),
+    isadmin boolean DEFAULT FALSE,
+    CHECK (isadmin = FALSE)
+);
+
+CREATE TABLE admin (
+    id SERIAL PRIMARY KEY,
     email VARCHAR(200) NOT NULL UNIQUE,
-    password VARCHAR(200)  NOT NULL
+    password VARCHAR(200) NOT NULL,
+    isadmin boolean DEFAULT TRUE,
+    CHECK (isadmin = TRUE)
 ); 
 
--- houses the award categorys --
+-- houses the award categories --
 CREATE TABLE category (
     id SERIAL PRIMARY KEY,
     full_category VARCHAR(200) NOT NULL UNIQUE
@@ -23,32 +57,27 @@ CREATE TABLE category (
 -- houses links to media files for a related ad --
 CREATE TABLE media (
     id SERIAL PRIMARY KEY,
-    ad_id INT REFERENCES ad (id) ON DELETE CASCADE,
     type INT,
-    url_one VARCHAR(200),
-    url_two VARCHAR(200),
-    url_three VARCHAR(200),
-    url_four VARCHAR(200),
-    url_five VARCHAR(200),
-    url_six VARCHAR(200)
+    url VARCHAR(200),
+    ad_id INT
 ); 
 
 -- houses ads with relations to the category and agency they're tied to --
 CREATE TABLE ad (
     id SERIAL PRIMARY KEY,
-	category_id INT REFERENCES category (id),
-    organization_id INT REFERENCES organization (id),
     advertiser VARCHAR(200),
     award VARCHAR(10),
-    year INT
+    year INT,
+    organization_id INT,
+    category_id INT
 ); 
 
 -- houses the people credited for producing a certain ad --
 CREATE TABLE credit (
     id SERIAL PRIMARY KEY,
-	ad_id INT REFERENCES ad (id) ON DELETE CASCADE,
     title VARCHAR(200),
-    name VARCHAR(200)
+    name VARCHAR(200),
+    ad_id INT
 ); 
 
 -- houses social media platform information --
@@ -61,7 +90,48 @@ CREATE TABLE social_media_type (
 -- houses social media links for agency profiles and the related social_media_type --
 CREATE TABLE social_media (
     id SERIAL PRIMARY KEY,
-    organization_id INT REFERENCES organization (id) ON DELETE CASCADE,
-    social_media_type_id INT REFERENCES social_media_type (id) ON DELETE CASCADE,
-    url VARCHAR(200)
-); 
+    url VARCHAR(200),
+    organization_id INT,
+    social_media_type_id INT
+
+);
+
+-- ** Add Foreign Key Constraints
+-- ad foreign keys
+ALTER TABLE ad
+    ADD CONSTRAINT organization_fk
+    FOREIGN KEY (organization_id)
+    REFERENCES organization (id);
+ALTER TABLE ad
+    ADD CONSTRAINT category_fk
+    FOREIGN KEY (category_id)
+    REFERENCES category (id);
+
+-- media foreign key
+ALTER TABLE media
+    ADD CONSTRAINT ad_fk
+    FOREIGN KEY (ad_id)
+    REFERENCES ad (id)
+    ON DELETE CASCADE;
+
+-- credit foreign key
+ALTER TABLE credit
+    ADD CONSTRAINT ad_fk
+    FOREIGN KEY (ad_id)
+    REFERENCES ad (id)
+    ON DELETE CASCADE;
+
+-- social media foreign key
+ALTER TABLE social_media
+    ADD CONSTRAINT organization_fk
+    FOREIGN KEY (organization_id)
+    REFERENCES organization (id)
+    ON DELETE CASCADE;
+ALTER TABLE social_media
+    ADD CONSTRAINT social_media_type_fk
+    FOREIGN KEY (social_media_type_id)
+    REFERENCES social_media_type (id)
+    ON DELETE CASCADE;
+
+-- ** Insert Mock Data **
+--
