ef5553a59a15e6c83672c529a22f00aa8ac29c24
Hunter Rancourt <skwid138@gmail.com>
2017-11-03 18:01:55 -0500

    setup basic home controller, auth controller, and auth service for testing

---
+++ public/scripts/controllers/home.controller.js
@@ -0,0 +1,11 @@
+/*
+This controller is for the home view.
+- it will do some things
+- and some other things
+*/
+
+myApp.controller('HomeController', function () {
+    console.log('in HomeController');
+    var vm = this;
+
+}); // end HomeController
---
+++ public/scripts/services/auth.service.js
@@ -0,0 +1,17 @@
+/*
+This Service is currently for user authentication
+- it will be used for testing and possible more
+*/
+
+myApp.service('AuthService', function ($http) {
+    console.log('in AuthService');
+    var self = this;
+
+
+
+
+    /************** $http **************/
+
+
+
+}); // end TrailService
