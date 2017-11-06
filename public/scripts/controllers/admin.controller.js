534dcb4af75cbc7918fae23afb3cacff171372dd
Alicia Wilking <aliciakaywilking@gmail.com>
2017-11-06 14:50:42 -0600

    connected the views and controller to the clientjs

---
+++ public/scripts/controllers/admin.controller.js
@@ -0,0 +1,22 @@
+myApp.controller('AdminController', function ($http, $location, $mdDialog) {
+    console.log('in AdminController');
+    var vm = this;
+
+    // user object for agency/organization login
+    vm.user = {
+        username: '',
+        password: ''
+    }; // end user object
+
+    // admin user object for admin login
+    vm.admin = {
+        username: '',
+        password: ''
+    }; // end admin object
+
+    // holds data from get USer
+    vm.getUserObj = {
+        email: '',
+        id: '',
+        isadmin: false
+    }; // end getUserObj
