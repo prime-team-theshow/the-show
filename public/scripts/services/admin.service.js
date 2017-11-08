3c642741f7c024c7187d153c34db8644176ba119
Hunter Rancourt <skwid138@gmail.com>
2017-11-08 10:22:15 -0600

    created nodemailer and admin services

---
+++ public/scripts/services/admin.service.js
@@ -0,0 +1,13 @@
+/*
+This Service is for admin related tasks
+- route: organizations to invite
+- route: add new year and background image
+- route: update year and background image
+- 
+*/
+
+myApp.service('AdminService', function ($http) {
+    console.log('in AdminService');
+    var self = this;
+
+}); // end AdminService
---
+++ public/scripts/services/test.nodemailer.service.js
@@ -0,0 +1,10 @@
+/*
+This Service is for node mailer and may be used after testing or merged into the admin service
+- it will be used for testing and possible more
+*/
+
+myApp.service('NodeMailerService', function ($http) {
+    console.log('in NodeMailerService');
+    var self = this;
+
+}); // end NodeMailerService
