a2c0d09967914be0657b56d7a3066f8d69833556
Hunter Rancourt <skwid138@gmail.com>
2017-11-09 11:07:21 -0600

    added basic get outline

--- routes/admin.router.js
+++ routes/admin.router.js
@@ -7,5 +7,10 @@ var express = require('express');
 var router = express.Router();
 var pool = require('../modules/pool');
 
+// GET organization that currently do not have an email or password
+router.get('/invite', function (req, res) {
+
+}); // end GET organizations to invite
+
 // export
 module.exports = router;
