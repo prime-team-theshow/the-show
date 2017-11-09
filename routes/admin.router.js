8107b2911ffebf20fa1ea0a130b44678bcd1e9d0
Hunter Rancourt <skwid138@gmail.com>
2017-11-09 10:18:11 -0600

    created admin route for serving admin related routes

---
+++ routes/admin.router.js
@@ -0,0 +1,11 @@
+/*
+This router handles admin related routes
+*/
+
+// requires
+var express = require('express');
+var router = express.Router();
+var pool = require('../modules/pool');
+
+// export
+module.exports = router;
